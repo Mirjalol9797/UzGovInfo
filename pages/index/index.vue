395909b4361c0f610b28045ca758e1eb782df024
Mirjalol <miramirxomitov97@gmail.com>
2024-11-08 18:02:28 +0500

    added text

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,13 @@
 <script setup>
 import useHeadquarters from "../../data/headquarters";
 import useMinistries from "../../data/ministries";
+import useState from "../../data/state";
 import Seo from "../../components/Seo/MainPageSeo.vue";
 
 const { t } = useI18n();
 const headquarters = useHeadquarters();
 const ministries = useMinistries();
+const state = useState();
 </script>
 
 <template>
@@ -44,7 +46,7 @@ const ministries = useMinistries();
       </section>
 
       <!-- ministries -->
-      <section>
+      <section class="mb-14">
         <div class="border-b border-[#eee] pb-2 flex-center-between mb-6">
           <h2 class="font-medium text-xl">
             {{ t("Ministries.headtitle") }}
@@ -56,6 +58,38 @@ const ministries = useMinistries();
             :to="`ministries/${item.slug}`"
             class="link-block"
             v-for="(item, index) in ministries.data"
+            :key="index"
+          >
+            <div class="min-w-[64px] h-[64px]">
+              <NuxtImg
+                :src="item.img"
+                :alt="t(item.title)"
+                :title="t(item.title)"
+                class="w-full h-full object-cover rounded-xl"
+                loading="lazy"
+                format="webp"
+              />
+            </div>
+            <h3 class="title">
+              {{ $t(item.title) }}
+            </h3>
+          </nuxt-link>
+        </div>
+      </section>
+
+      <!-- state -->
+      <section class="mb-14">
+        <div class="border-b border-[#eee] pb-2 flex-center-between mb-6">
+          <h2 class="font-medium text-xl">
+            {{ t("Ministries.headtitle") }}
+          </h2>
+          <nuxt-link to="/" class="text-xs">{{ t("see_all") }}</nuxt-link>
+        </div>
+        <div class="grid grid-cols-4 gap-4">
+          <nuxt-link
+            :to="`ministries/${item.slug}`"
+            class="link-block"
+            v-for="(item, index) in state.data"
             :key="index"
           >
             <div class="min-w-[64px] h-[64px]">
