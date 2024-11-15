ab3b0e657d55bb6d71d6626ae69fad758628bdff
Mirjalol <miramirxomitov97@gmail.com>
2024-11-15 15:39:45 +0500

    change code

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,6 +20,18 @@ if (process.client) {
   gtag("js", new Date());
 
   gtag("config", "G-0K2W3VD5PG");
+
+  // <!-- Google Tag Manager -->
+  (function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-K6LQ2SBW");
 }
 
 function siteScrollTop() {
@@ -75,20 +87,3 @@ onMounted(() => {
   flex: 1;
 }
 </style>
-
-<template #head>
-  <!-- Google Tag Manager -->
-  <script>
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-      var f = d.getElementsByTagName(s)[0],
-        j = d.createElement(s),
-        dl = l != "dataLayer" ? "&l=" + l : "";
-      j.async = true;
-      j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-      f.parentNode.insertBefore(j, f);
-    })(window, document, "script", "dataLayer", "GTM-K6LQ2SBW");
-  </script>
-  <!-- End Google Tag Manager -->
-</template>
