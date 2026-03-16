e9016de9b9b57c02f7f6ee19697ad361c722793e
Mirjalol Mirxomitov <miramirxomitov97@gmail.com>
2026-03-16 15:13:00 +0500

    footer

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,4 +1,15 @@
-<script setup></script>
+<script setup>
+const { locale } = useI18n();
+
+const organizationsLinkText = computed(() => {
+  const map = {
+    ru: "Все организации",
+    en: "All organizations",
+    uz: "Barcha tashkilotlar",
+  };
+  return map[locale.value] || map.ru;
+});
+</script>
 
 <template>
   <footer class="bg-[#F3F4F9] mt-10 py-4">
@@ -7,7 +18,7 @@
         class="flex justify-between items-center 960:flex-col 960:items-start gap-2"
       >
         <div class="text-sm text-[#111827]">
-          Gos sayts 2024. {{ $t("all_rights_reserved") }}
+          © 2025-2026 govinfo.uz. {{ $t("all_rights_reserved") }}
         </div>
         <div
           class="flex items-center gap-4 text-sm 640:flex-col 640:items-start 640:gap-2"
@@ -16,7 +27,7 @@
             :to="localePath('/organizations')"
             class="text-[#1C335F] hover:text-[#315EFB]"
           >
-            {{ $t("Organizations.headtitle") }}
+            {{ organizationsLinkText }}
           </nuxt-link>
           <nuxt-link
             :to="localePath('/about')"
